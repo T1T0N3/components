53f39cac63d891ead74a5d5959123c65414efd7f
T1T0N3 <115908075+T1T0N3@users.noreply.github.com>
2024-07-04 16:44:35 +0200

    Update laketree-progressbar.js

--- laketree-progressbar.js
+++ laketree-progressbar.js
@@ -5,240 +5,43 @@ export class LaketreeProgressbar extends LitElement {
 
   static styles = css` 
   
-  .container {
-    width: 100%;
-    background-color: #ddd;
-    border-radius:10px;    
-  }
-  
-  .laketree-pbar {
-    border-radius:10px;    
-    text-align: right;
-    background-color: var(--ntx-form-theme-color-primary);
-    color: var(--ntx-form-theme-color-form-background);
-  }
+  .container{
+    width: 110px;
+    height: 110px;
+    margin: 100px auto;
+}
+.prec{
+    top: 30px;
+    position: relative;
+    font-size: 30px;
+}
 
-   @import "compass";
-      @import url('https://fonts.googleapis.com/css?family=Titillium+Web:200,200i,300,300i,400,400i,600,600i,700,700i,900');
+.circle{
+    position: relative;
+    top: 5px;
+    left: 5px;
+    text-align: center;
+    width: 100px;
+    height: 100px;
+    border-radius: 100%;
+    background-color: #E6F4F7;
+}
 
-      body {
-        font-family: "Titillium Web", sans-serif;
-        margin: 0 auto;
-      }
+.active-border{
+    position: relative;
+    text-align: center;
+    width: 110px;
+    height: 110px;
+    border-radius: 100%;
 
-      // VARS
-      $circle-width: 0.09em;
-      $circle-width-hover: 0.07em;
+    background-color:#39B4CC;
+    background-image:
+        linear-gradient(91deg, transparent 50%, #A2ECFB 50%),
+        linear-gradient(90deg, #A2ECFB 50%, transparent 50%);
+    
+}
 
-      // colors default
-      $primary-color: #000000; // czarny
-      $secondary-color: #dfe8ed; //szary bcg
-      $bg-color: #ffffff; //srodkowy bezowy 
-
-      // colors customized
-      $primary-color-blue: #30bae7;
-      $primary-color-green: #15c7a8;
-      $primary-color-orange: #eb7d4b; 
-      $primary-color-pink: #d74680; 
-      $primary-color-span: #3c4761;
-
-      // CIRCLE
-      // classes 2 extend
-      .rect-auto{
-        clip: rect(auto, auto, auto, auto);
-      }
-
-      .pie {
-        position: absolute;
-        border: $circle-width solid $primary-color;
-        width: 1 - (2 * $circle-width);
-        height: 1 - (2 * $circle-width);
-        clip: rect(0em, 0.5em, 1em, 0em);
-        border-radius: 50%;
-        @include rotate(0deg);
-      }
-
-      .pie-fill {
-        @include rotate(180deg);
-      }
-      .wrapper {
-        width: 1200px;
-        margin: 0 auto;
-      }
-
-
-      // main
-      .c100 {
-
-        *, *:before, *:after {
-          @include box-sizing(content-box);
-        }
-
-        position: relative;
-        font-size: 160px;
-        width: 1em;
-        height: 1em;
-        border-radius: 50%;
-        float: left;
-        margin: 0.4em;
-        background-color: $secondary-color;
-
-      // 	// center circle to its parent
-      // 	&.center{
-      // 		float: none;
-      // 		margin: 0 auto;
-      // 	}
-
-      // 	&.small{
-      // 		font-size: 80px;
-      // 	}
-
-        // centered value inside circle
-        > span {
-          position: absolute;
-          width: 100%;
-          z-index: 1;
-          left: 0;
-          top: 0;
-          width: 5em;
-          line-height: 5em;
-          font-size: 0.2em;
-          color: $primary-color-span;
-          display: block;
-          text-align: center;
-          white-space: nowrap;
-          @include transition-property(all);
-          @include transition-duration(0.2s);
-          @include transition-timing-function(ease-out);
-        }
-
-        // background inside the circle
-        &:after{
-          position: absolute;
-          top: $circle-width;
-          left: $circle-width;
-          display: block;
-          content: " ";
-          border-radius: 50%;
-          background-color: $bg-color;
-          width: 1 - (2 * $circle-width);
-          height: 1 - (2 * $circle-width);
-          @include transition-property(all);
-          @include transition-duration(0.2s);
-          @include transition-timing-function(ease-in);
-
-        }
-
-        // the slice (mask)
-        .slice {
-          position: absolute;
-          width: 1em;
-          height: 1em;
-          clip: rect(0em, 1em, 1em, 0.5em);
-        }
-
-        // circle to show the status
-        .bar {
-          @extend .pie;
-        }
-
-
-        // loop to create all needed elements automatically
-        @for $j from 51 through 100 {
-
-          &.p#{$j} .slice {
-            @extend .rect-auto;
-          }
-
-          &.p#{$j} .bar:after{
-            @extend .pie-fill;
-          }
-
-          &.p#{$j} .fill{
-            @extend .pie;
-            @extend .pie-fill;
-          }
-
-        }
-
-        // loop to rotate all 100 circles
-        @for $j from 1 through 100 {
-          &.p#{$j} .bar {
-            @include rotate((360/100*$j) + deg);
-          }
-        }
-
-
-
-        // hover styles
-        &:hover{
-
-          cursor: default;
-
-          > span {
-            width: 3.33em;
-            line-height: 3.33em;
-            font-size: 0.3em;
-            color: $primary-color-span;
-          }
-
-          &:after{
-            top: $circle-width-hover;
-            left: $circle-width-hover;
-            width: 1 - (2 * $circle-width-hover);
-            height: 1 - (2 * $circle-width-hover);
-          }
-
-        }
-        
-        // blue
-        &.blue{
-
-          .bar, .fill { border-color: $primary-color-blue !important;}
-
-          &:hover{
-            > span { color: $primary-color-span;}
-          }
-
-        }
-
-
-        // pink skin
-        &.pink{
-
-          .bar, .fill { border-color: $primary-color-pink !important;}
-
-          &:hover{
-            > span { color: $primary-color-span;}
-          }
-
-        }
-
-
-        // green skin
-        &.green{
-
-          .bar, .fill { border-color: $primary-color-green !important;}
-
-          &:hover{
-            > span { color: $primary-color-span;}
-          }
-
-        }
-
-
-        // orange skin
-        &.orange{
-
-          .bar, .fill { border-color: $primary-color-orange !important;}
-
-          &:hover{
-            > span { color: $primary-color-span;}
-          }
-
-        }
-
-      }
+   
     
   `
 
@@ -272,6 +75,44 @@ export class LaketreeProgressbar extends LitElement {
     };
   }
 
+var i = 0 , prec;
+var degs = $("#prec").attr("class").split(' ')[1];
+var activeBorder = $("#activeBorder");
+
+setTimeout(function(){
+    if($("#circle").is(":hover"))
+       loopit("c");
+    else
+       loopit("nc");
+},1);
+
+loopit(dir){
+    if (dir == "c")
+        i++
+    else
+        i--;
+    if (i < 0)
+        i = 0;
+    if (i > degs)
+        i = degs;
+    prec = (100*i)/360;   
+    $(".prec").html(Math.round(prec)+"%");
+    
+    if (i<=180){
+        activeBorder.css('background-image','linear-gradient(' + (90+i) + 'deg, transparent 50%, #A2ECFB 50%),linear-gradient(90deg, #A2ECFB 50%, transparent 50%)');
+    }
+    else{
+        activeBorder.css('background-image','linear-gradient(' + (i-90) + 'deg, transparent 50%, #39B4CC 50%),linear-gradient(90deg, #A2ECFB 50%, transparent 50%)');
+    }
+    
+    setTimeout(function(){
+        if($("#circle").is(":hover"))
+            loopit("c");
+        else
+           loopit("nc");
+    },1);
+    
+}
 
   constructor() {
     super();
@@ -309,15 +150,13 @@ export class LaketreeProgressbar extends LitElement {
 
     </style>
     
-  <div class="wrapper">
-    <div class="c100 ${this._class} blue">
-      <span>${this._displayValue} ${this._class}</span>
-      <div class="slice">
-        <div class="bar"></div>
-        <div class="fill"></div>
-      </div>
-  </div>
-  </div>
+  <div class="container">
+    <div id="activeBorder" class="active-border">
+        <div id="circle" class="circle">
+            <span class="prec 270" id="prec">0%</span>
+        </div>
+    </div>
+</div>
 
         `;
   }
