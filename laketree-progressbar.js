e8a1b8c8bcf70e11874b688a23555b3169b0e686
T1T0N3 <115908075+T1T0N3@users.noreply.github.com>
2024-07-05 08:58:46 +0200

    Update laketree-progressbar.js

--- laketree-progressbar.js
+++ laketree-progressbar.js
@@ -3,6 +3,7 @@ import {
   css,
   LitElement,
 } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+
 // define the component
 export class LaketreeProgressbar extends LitElement {
   static styles = css`
@@ -11,13 +12,18 @@ export class LaketreeProgressbar extends LitElement {
     }
 
     .container {
-      width: 100%;
+      width: 300px; /* Aumentato */
+      height: 300px; /* Aumentato */
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
     }
 
     .circle {
       position: relative;
-      width: 100px;
-      height: 100px;
+      width: 250px; /* Aggiornato per garantire un margine */
+      height: 250px; /* Aggiornato per garantire un margine */
     }
 
     .circle svg {
@@ -27,14 +33,16 @@ export class LaketreeProgressbar extends LitElement {
     .circle circle {
       fill: none;
       stroke-width: 10;
+      width: 100%;
+      height: auto;
       stroke: var(--circle-bg-color, #e6e6e6);
     }
 
     .circle .progress {
       stroke: var(--circle-color, #4caf50);
       stroke-linecap: round;
-      stroke-dasharray: 314;
-      stroke-dashoffset: 314;
+      stroke-dasharray: 625; /* Aggiornato per la nuova circonferenza */
+      stroke-dashoffset: 625; /* Aggiornato per la nuova circonferenza */
       transition: stroke-dashoffset 1s ease;
     }
 
@@ -91,7 +99,7 @@ export class LaketreeProgressbar extends LitElement {
   }
 
   updateCircle() {
-    const radius = 50;
+    const radius = 100; // Aggiornato per la nuova dimensione
     const circumference = 2 * Math.PI * radius;
     const offset = circumference - (this.score / 100) * circumference;
     this.shadowRoot.querySelector('.progress').style.strokeDashoffset = offset;
@@ -101,9 +109,10 @@ export class LaketreeProgressbar extends LitElement {
     return html`
       <div class="container">
         <div class="circle">
-          <svg width="100" height="100">
-            <circle cx="50" cy="50" r="50"></circle>
-            <circle class="progress" cx="50" cy="50" r="50"></circle>
+          <svg width="250" height="250">
+            <!-- Aggiornato -->
+            <circle class="progress" cx="125" cy="125" r="100"></circle>
+            <!-- Aggiornato -->
           </svg>
           <div class="number">${this.score}%</div>
         </div>
